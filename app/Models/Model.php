bd93e318d1844278d2d1be2b85c5112edfb75edc
emoc lagaillarde <emoc11@free.fr>
2014-02-18 11:14:22 +0100

    Changement CSS (chinois -> normal) + google API

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -6,10 +6,10 @@ protected $model;
   
 	function __construct(){
 		$f3=\Base::instance();
-		$this->dB=new \DB\SQL('mysql:host='.$f3->get('db_host').';port=8889;dbname='.$f3->get('db_server'),$f3->get('db_login'),$f3->get('db_password'));
+		//$this->dB=new \DB\SQL('mysql:host='.$f3->get('db_host').';port=8889;dbname='.$f3->get('db_server'),$f3->get('db_login'),$f3->get('db_password'));
 
 		// CONNEXION ONLINE
-		//$this->dB=new \DB\SQL('mysql:host=localhost;dbname=u&me','root','010090');
+		$this->dB=new \DB\SQL('mysql:host=localhost;dbname=u&me','root','010090');
 	}
 
 	function getMapper($table){
