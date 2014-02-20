8d9489f72b77e125bf05e61bcf1b3147e1a7a177
emoc lagaillarde <emoc11@free.fr>
2014-02-20 15:40:36 +0100

    Verif calendar + ajout si absent + event ou non

--- public/js/calendrier.js
+++ public/js/calendrier.js
@@ -26,41 +26,69 @@ var clientId = '1094307263361-6q6u8m0ljv8fdor650pji75a7m5sjbfh.apps.googleuserco
           return false;
         }
 
-        // Load the API and make an API call.  Display the results on the screen.
         function makeApiCall() {
-
-          // Step 4: Load the Calendar API
+          // Calendar API v3
           gapi.client.load('calendar', 'v3', function() {
 
-            // Step 5: Assemble the API request
-            var date = new Date();
-            var request_show = gapi.client.calendar.events.list({
-              'calendarId': 'come.gaillard@hetic.net',
-              'startDate': date
+            // Calendrier déjà présent ? --------------------
+            var calendrier_present = false;
+            var test_calendrier = false;
+            var calendrier_uandme_id;
+            var request_calendar_list = gapi.client.calendar.calendarList.list();
+            // Effectue la recherche
+            request_calendar_list.execute(function(resp){
+              for (var i = 0; i < resp.items.length; i++){
+                if(resp.items[i].summary == "u&me"){
+                  calendrier_uandme_id = resp.items[i].id;
+                  calendrier_present = true;
+                }
+                test_calendrier = true;
+              }
+              // Calendrier absent -> création
+              if(!calendrier_present && test_calendrier){
+                create_new_uandme();
+              }
+              // Calendrier présent -> récup evenements
+              else {
+                recup_events_uandme(calendrier_uandme_id);
+              }
             });
 
-            // Step 6: Execute the API request
-            request_show.execute(function(resp) {
-              for (var i = 0; i < resp.items.length; i++) {
-                var li = document.createElement('li');
-                  li.appendChild(document.createTextNode(resp.items[i].summary));
-                  if(resp.items[i].start != undefined && resp.items[i].start != "undefined")
+            function create_new_uandme() {
+              var request_calendar_insert = gapi.client.calendar.calendars.insert({
+                "resource" :{
+                  "summary": "u&me",
+                  "description": "Généré par www.u&me.eu",
+                  "location" : "France",
+                  "timezone": "France/Paris"
+                }
+              });
+              var test = request_calendar_insert.execute()
+            }
+
+            function recup_events_uandme(calendrier_id) {
+              // Recherche des events suivants sur le calendrier
+              var date = new Date();
+              var request_show = gapi.client.calendar.events.list({
+                'calendarId': calendrier_id
+              });
+
+              // Step 6: Execute the API request
+              request_show.execute(function(resp) {
+                if(resp.items != undefined) {
+                  for (var i = 0; i < resp.items.length; i++) {
+                  var li = document.createElement('li');
+                    li.appendChild(document.createTextNode(resp.items[i].summary));
                     li.appendChild(document.createTextNode(resp.items[i].start.dateTime));
-                  document.getElementById('events').appendChild(li);
-              }
-            });
-
-            var request_calendar_insert = gapi.client.calendar.calendars.insert({
-              "resource" :{
-                "summary": "u&me",
-                "description": "Généré par www.u&me.eu",
-                "location" : "France",
-                "timezone": "France/Paris"
-              }
-            });
-
-            // Step 6: Execute the API request
-            var test = request_calendar_insert.execute()
+                    document.getElementById('events').appendChild(li);
+                  }
+                } else {
+                  var erreur = document.createElement('p');
+                  erreur.innerHTML = "Aucun évènement trouvé.";
+                  document.getElementById('events').appendChild(erreur);
+                }
+              });
+            }
 
             // CALENDAR HERE
 
